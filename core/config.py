899f1b1196ede7d86d63cbf563ec841258b13761
Nitin Singh <nitin_2312res424@iitp.ac.in>
2025-08-18 15:38:23 +0530

    add requirements.txt

--- core/config.py
+++ core/config.py
@@ -7,14 +7,14 @@ load_dotenv()
 class Config:
     @property
     def GEMINI_API_KEY(self) -> str:
-        value = os.getenv("GEMINI_API_KEY")
+        value = os.getenv("AIzaSyB1WbRxcGG-BlaFiBkhQTw7h9PVyBXKVm4")
         if not value:
             raise ValueError("GEMINI_API_KEY environment variable is required.")
         return value
 
     @property
     def QDRANT_URL(self) -> str:
-        return os.getenv("QDRANT_URL", "http://localhost:6333")
+        return os.getenv("QDRANT_URL", "https://be012654-1dc6-4856-950d-04fc5edff456.us-east4-0.gcp.cloud.qdrant.io:6333")
 
     @property
     def QDRANT_COLLECTION(self) -> str:
